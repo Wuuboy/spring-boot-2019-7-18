d1bee13176ad61ee2f9e355ea0b3570c11842f9b
曹曹 <1992942606@qq.com>
2019-07-18 21:53:16 +0800

    [Demi]feat:config ha and flyway

---
+++ src/main/java/com/example/demo/ParkingLot.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+@Entity
+public class ParkingLot {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    @Column
+    private  Integer capacity;
+    private String location;
+
+    public ParkingLot() {
+    }
+
+    public ParkingLot(String name, Integer capacity, String location) {
+        this.name = name;
+        this.capacity = capacity;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/main/resources/db/migration/V2__create_table_parkinglot.sql
@@ -0,0 +1,4 @@
+drop table parking_lot if exists;
+drop sequence if exists hibernate_sequence;
+create sequence hibernate_sequence start with 1 increment by 1;
+create table parking_lot (id bigint not null, capacity integer, location varchar(255), name varchar(255), primary key (id));
